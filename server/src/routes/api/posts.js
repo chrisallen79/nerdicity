3893c254ee61303d5f258b926554f25bc1a4f8a9
chrisallen79 <chrisallen25@gmail.com>
2020-01-29 21:41:32 -0500

    [NERD-3] Adding basic post routes.

--- server/src/routes/api/posts.js
+++ server/src/routes/api/posts.js
@@ -1,12 +1,154 @@
-import express from 'express';
+import express, { request } from 'express';
+import { check, validationResult } from 'express-validator';
+import { auth } from '../../middleware/auth';
+import { Post } from '../../models/Post';
+import { Profile } from '../../models/Profile';
+import { User } from '../../models/User';
 
 const router = express.Router();
 
+// @route   POST api/posts
+// @desc    Create a post
+// @access  Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    }
+  }
+);
+
 // @route   GET api/posts
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => {
-  res.send('Posts route');
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by ID
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({ msg: `Post with ID ${req.params.id} not found` });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(404).json({ msg: `Post with ID ${req.params.id} not found` });
+    }
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete post by ID
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: `Post with ID ${req.params.id} not found` });
+    }
+
+    // make sure user owns post before deleting
+    if (post.user != req.user.id) {
+      return res.status(401).json({ msg: `User not authorized` });
+    }
+
+    await post.remove();
+
+    res.json({ msg: `Post deleted` });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(404).json({ msg: `Post with ID ${req.params.id} not found` });
+    }
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// @route   PUT api/posts/like/:id
+// @desc    Like a post
+// @access  Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.likes.filter(like => like.user == req.user.id).length > 0) {
+      return res.status(400).json({ msg: `Post already liked` });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// @route   PUT api/posts/unlike/:id
+// @desc    Unlike a post
+// @access  Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.likes.filter(like => like.user == req.user.id).length === 0) {
+      return res.status(400).json({ msg: `Post has not been liked` });
+    }
+
+    const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 export default router;

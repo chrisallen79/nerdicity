3743bfaf41b607916c8c3c0d9e58e3cdd2763b6b
chrisallen79 <chrisallen25@gmail.com>
2020-01-30 21:44:01 -0500

    [NERD-3] Added add/delete comment routes to posts

--- server/src/routes/api/posts.js
+++ server/src/routes/api/posts.js
@@ -151,4 +151,77 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
+// @route   POST api/posts/comment/:id
+// @desc    Comment on a post
+// @access  Private
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    }
+  }
+);
+
+// @route   DELETE api/posts/comment/:id/:commentId
+// @desc    Delete a comment
+// @access  Private
+router.delete('/comment/:id/:commentId', auth, async (req, res) => {
+  try {
+    // find the post and comment
+    const post = await Post.findById(req.params.id);
+    const comment = post.comments.find(comment => comment.id == req.params.commentId);
+
+    // make sure the comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: `Comment ${req.params.commentId} does not exist` });
+    }
+
+    // make sure the user can delete this comment
+    if (comment.user != req.user.id) {
+      return res.status(401).json({ msg: `User is not authorized to delete this comment` });
+    }
+
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 export default router;

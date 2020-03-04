67b48278ee6282266dfee54a3c2969a29a15bba2
chrisallen79 <chrisallen25@gmail.com>
2020-03-03 21:47:32 -0500

    [CBBC-19] Implemented rudimentary flexbox design.

--- client/src/components/layout/Main.js
+++ client/src/components/layout/Main.js
@@ -4,24 +4,22 @@ import { Button, Container, Grid, Image, Header, Segment } from 'semantic-ui-rea
 
 const Main = () => {
   return (
-    <section className="bg-halftone">
-      <div className="home-inner">
-        <div className="home-image">
-          <img src="/assets/cbbc-logo-light.svg" />
-        </div>
-        <Container>
-          Join a group and discuss your favorite books, creators, shows, and more!!!
-        </Container>
-        <div style={{ paddingTop: '30px' }}>
-          <Button as={Link} to="/register" secondary>
-            Register
-          </Button>
-          <Button as={Link} to="/login" primary>
-            Login
-          </Button>
-        </div>
+    <div>
+      <div className="home-image">
+        <img src="/assets/cbbc-logo-light.svg" />
       </div>
-    </section>
+      <Container>
+        Join a group and discuss your favorite books, creators, shows, and more!!!
+      </Container>
+      <div style={{ paddingTop: '30px' }}>
+        <Button as={Link} to="/register" secondary>
+          Register
+        </Button>
+        <Button as={Link} to="/login" primary>
+          Login
+        </Button>
+      </div>
+    </div>
   );
 };
 

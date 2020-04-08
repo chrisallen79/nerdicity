4993147b7412cc71919dcdcda3b2c7a1965493db
chrisallen79 <chrisallen25@gmail.com>
2020-04-07 21:12:53 -0400

    Corrected footer spacing issues

--- app/client/src/components/layout/Footer.js
+++ app/client/src/components/layout/Footer.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Container } from 'semantic-ui-react';
 
 const Footer = () => {
+  const copyYear = new Date().getFullYear();
+
   return (
     <div id="footer">
       <Container>
-        Copyright &copy;
-        {new Date().getFullYear()}
-        Nerdicity Media
+        <span>Copyright &copy;&nbsp;</span>
+        <span>{copyYear}</span>
+        <span>&nbsp;Nerdicity Media</span>
       </Container>
     </div>
   );
